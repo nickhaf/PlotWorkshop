93ebaf343fcf1e714c37f576066c3bab1efb399c
Nicklas Hafiz <hafiznij@hu-berlin.de>
2025-03-13 12:58:40 +0100

    Example plot mass shootings

--- src/themes.R
+++ src/themes.R
@@ -1,12 +1,14 @@
 
 theme_bg <- function() {
-  theme_classic() +
+  theme_minimal() +
     theme(
       strip.background = element_rect(fill="#F8F8F8", colour = '#01364C'), 
       panel.background = element_rect(fill = '#F8F8F8', colour = '#F8F8F8'),
           plot.background = element_rect(fill = '#F8F8F8', colour = '#F8F8F8'),
-          text = element_text(colour = '#01364C'), 
+          text = element_text(colour = '#01364C', size = 14), 
           axis.line = element_line(colour = '#01364C'), 
           axis.ticks = element_line(colour = '#01364C'), 
-          axis.text = element_text(colour = '#01364C'))
+          axis.text = element_text(colour = '#01364C', size = 14), 
+      panel.grid.major = element_line(colour = "grey92", size = 0.5), 
+      panel.grid.minor = element_blank())
 }
