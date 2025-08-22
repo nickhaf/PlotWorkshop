d286e3be25c981f59e23fd3bd9a17faea8115825
Nicklas Hafiz <nicklashaf@gmail.com>
2025-08-22 13:38:47 +0200

    Add link to repo

--- data/prep_gapminder_dat.R
+++ data/prep_gapminder_dat.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 source(here::here("src", "functions.R"))
 
-gapminder_dat_full <- get_gapminder(keywords = c("pop--", "co2", "--geo--country", "--lex"))
+gapminder_dat_full <- get_gapminder(repo = "https://github.com/open-numbers/ddf--gapminder--fasttrack",
+                                    keywords = c("pop--", "co2", "--geo--country", "--lex"))
+
 #, "--lex", "--gdp", "--mincpcap", "employ", "--geo", "--hapi", "demox", "--gini"))
 # life expectancy, gdp, minimum income per capita, employment, geography, happiness, democraty index, gini index 
 
