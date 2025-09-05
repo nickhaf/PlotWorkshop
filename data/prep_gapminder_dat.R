305df234edbbb2672b1b7f5f31fbc81f86d4f53d
nickhaf <nicklashaf@gmail.com>
2025-09-05 14:39:38 +0200

    Add variables to dataset

--- data/prep_gapminder_dat.R
+++ data/prep_gapminder_dat.R
@@ -2,9 +2,9 @@ library(tidyverse)
 source(here::here("src", "functions.R"))
 
 gapminder_dat_full <- get_gapminder(repo = "https://github.com/open-numbers/ddf--gapminder--fasttrack",
-                                    keywords = c("pop--", "co2", "--geo--country", "--lex"))
+                                    keywords = c("pop--", "co2", "--geo--country", "--lex", "--hapi", "demox", "--gini", "employ", "--gdp"))
 
-#, "--lex", "--gdp", "--mincpcap", "employ", "--geo", "--hapi", "demox", "--gini"))
+#, "--mincpcap", "--geo",)
 # life expectancy, gdp, minimum income per capita, employment, geography, happiness, democraty index, gini index 
 
 saveRDS(gapminder_dat_full, here::here("data", "gapminder_dat.rds"))
