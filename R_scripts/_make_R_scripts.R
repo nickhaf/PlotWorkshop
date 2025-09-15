19af619a400c417a7fe3e8abde7d8aa8278b62f9
buchjani <janine.buchholz@hu-berlin.de>
2025-09-15 11:50:52 +0200

    removed Evaluation link

---
+++ R_scripts/_make_R_scripts.R
@@ -0,0 +1,4 @@
+library(knitr)
+purl(input = "./sessions/animate_plotly/slides.qmd", output = "./R_scripts/animate_plotly.R")
+purl(input = "./sessions/science_plots/slides.qmd", output = "./R_scripts/science_plots.R")
+purl(input = "./sessions/exercise/slides.qmd", output = "./R_scripts/exercise.R")
